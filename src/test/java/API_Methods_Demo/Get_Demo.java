f9d970f69ebee047af1304118ba67fba94ee6bcd
shankkalra <shankkalra@gmail.com>
2021-12-23 22:34:00 +0530

    Validate Response

--- src/test/java/API_Methods_Demo/Get_Demo.java
+++ src/test/java/API_Methods_Demo/Get_Demo.java
@@ -5,20 +5,38 @@ import com.rest.propertyReader.ObjectReader;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.hasItems;
 
 public class Get_Demo {
 
     @Test
-    public void validate_Get_Verb(){
+    public void validate_Get_Verb() {
         given()
                 .baseUri(ObjectReader.reader.getURI())
-                .header("x-api-key",ObjectReader.reader.getKey())
-        .when()
+                .header("x-api-key", ObjectReader.reader.getKey())
+                .when()
                 .log().method()
                 .get(Endpoints_Web_Services.WORKSPACE)
-        .then()
+                .then()
                 .log().all()
                 .assertThat()
                 .statusCode(200);
+    }
+
+    @Test
+    public void validate_Response_Body() {
+        given()
+                .baseUri(ObjectReader.reader.getURI())
+                .header("x-api-key", ObjectReader.reader.getKey())
+                .when()
+                .log().method()
+                .get(Endpoints_Web_Services.WORKSPACE)
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .body("workspaces.name", hasItems("My Workspace", "Team Workspace", "Dummy_New_Workspace"),
+                        "workspaces.type", hasItems("personal", "team", "team"));
+
     }
 }
