baddb921a59a238ffe3cd2b4f9321588d7b581e5
shankkalra <shankkalra@gmail.com>
2021-12-23 20:23:04 +0530

    Adding GitIgnore File

---
+++ src/test/java/com/rest/Test.java
@@ -0,0 +1,14 @@
+package com.rest;
+
+import static io.restassured.RestAssured.given;
+
+public class Test {
+
+    @org.testng.annotations.Test
+    public void test(){
+        given()
+                .when()
+                .then();
+    }
+
+}
