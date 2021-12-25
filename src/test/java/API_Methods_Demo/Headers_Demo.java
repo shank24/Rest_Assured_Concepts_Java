46497d4055ade958c4e844c33194373a05f22d5b
shankkalra <shankkalra@gmail.com>
2021-12-25 19:18:52 +0530

    Assert Response Headers

---
+++ src/test/java/API_Methods_Demo/Headers_Demo.java
@@ -0,0 +1,27 @@
+package API_Methods_Demo;
+
+import com.rest.endpoints.Endpoints_Web_Services;
+import com.rest.propertyReader.ObjectReader;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+
+public class Headers_Demo {
+
+    @Test
+    public void assert_Response_Headers() {
+        given()
+                .baseUri(ObjectReader.reader.getURI())
+                .header("x-api-key", ObjectReader.reader.getKey())
+                .when()
+                .log().method()
+                .get(Endpoints_Web_Services.WORKSPACE)
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .header("X-RateLimit-Limit", "300");
+    }
+
+}
