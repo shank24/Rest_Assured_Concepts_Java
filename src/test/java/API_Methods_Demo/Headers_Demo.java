8484af47803bc23c9d38a5ade33f7cbf17fbe058
shankkalra <shankkalra@gmail.com>
2021-12-25 19:24:48 +0530

    Extract Multiple Response Headers

--- src/test/java/API_Methods_Demo/Headers_Demo.java
+++ src/test/java/API_Methods_Demo/Headers_Demo.java
@@ -2,6 +2,7 @@ package API_Methods_Demo;
 
 import com.rest.endpoints.Endpoints_Web_Services;
 import com.rest.propertyReader.ObjectReader;
+import io.restassured.http.Headers;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -25,4 +26,23 @@ public class Headers_Demo {
                         "Server", "nginx");
     }
 
+    @Test
+    public void extract_Response_Headers() {
+        Headers extracted_Headers = given()
+                .baseUri(ObjectReader.reader.getURI())
+                .header("x-api-key", ObjectReader.reader.getKey())
+                .when()
+                .log().method()
+                .get(Endpoints_Web_Services.WORKSPACE)
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .headers();
+
+        System.out.println("Extracted Headers Name --> " + extracted_Headers.get("Server").getName());
+        System.out.println("Extracted Headers Value --> " + extracted_Headers.get("Server").getValue());
+
+    }
 }
