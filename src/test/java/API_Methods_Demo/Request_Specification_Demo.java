5d7784799eda52066cc3bfc709f13ff80bb25e9b
shankkalra <shankkalra@gmail.com>
2021-12-25 23:35:56 +0530

    Request_Spec_Builder

--- src/test/java/API_Methods_Demo/Request_Specification_Demo.java
+++ src/test/java/API_Methods_Demo/Request_Specification_Demo.java
@@ -2,20 +2,21 @@ package API_Methods_Demo;
 
 import com.rest.endpoints.Endpoints_Web_Services;
 import com.rest.propertyReader.ObjectReader;
+import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.get;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class Request_Specification_Demo {
-    RequestSpecification requestSpecification;
+
+    //RequestSpecification requestSpecification;
 
     @BeforeClass
     public void beforeClass() {
@@ -29,17 +30,16 @@ public class Request_Specification_Demo {
         requestSpecBuilder.addHeader("x-api-key", ObjectReader.reader.getKey());
         requestSpecBuilder.log(LogDetail.ALL);
 
-        requestSpecification = requestSpecBuilder.build();
+        //This static variable will retains its value,
+        // so we can directly use get()
+        RestAssured.requestSpecification = requestSpecBuilder.build();
     }
 
     @Test
 
     public void request_Spec_Demo() {
 
-        given().spec(requestSpecification)
-                .when()
-                .log().method()
-                .get(Endpoints_Web_Services.WORKSPACE)
+        get(Endpoints_Web_Services.WORKSPACE)
                 .then()
                 .log().all()
                 .assertThat()
@@ -50,10 +50,7 @@ public class Request_Specification_Demo {
     public void log_request_Body() {
 
 
-        given().spec(requestSpecification)
-                .when()
-                .log().all()
-                .get(Endpoints_Web_Services.WORKSPACE)
+        get(Endpoints_Web_Services.WORKSPACE)
                 .then()
                 .log().all()
                 .assertThat()
@@ -64,14 +61,14 @@ public class Request_Specification_Demo {
     @Test
     public void assert_Status_Code() {
 
-        Response response = given().spec(requestSpecification).get(Endpoints_Web_Services.WORKSPACE).then().log().all().extract().response();
+        Response response = get(Endpoints_Web_Services.WORKSPACE).then().log().all().extract().response();
         assertThat(response.statusCode(), is(equalTo(200)));
     }
 
     @Test
     public void assert_Body() {
 
-        Response response = given().spec(requestSpecification).get(Endpoints_Web_Services.WORKSPACE).then().log().all().extract().response();
+        Response response = get(Endpoints_Web_Services.WORKSPACE).then().log().all().extract().response();
         assertThat(response.path("workspaces[0].name"), is(equalTo("My Workspace")));
     }
 
