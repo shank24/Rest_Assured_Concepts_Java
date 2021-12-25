84b033f2889c05bf46906229a2c114e12f7a28e8
shankkalra <shankkalra@gmail.com>
2021-12-25 23:58:35 +0530

    Response_Spec

---
+++ src/test/java/API_Methods_Demo/Response_Specification_Demo.java
@@ -0,0 +1,68 @@
+package API_Methods_Demo;
+
+import com.rest.endpoints.Endpoints_Web_Services;
+import com.rest.propertyReader.ObjectReader;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class Response_Specification_Demo {
+
+    //RequestSpecification requestSpecification;
+
+    ResponseSpecification responseSpecification;
+
+    @BeforeClass
+    public void beforeClass() {
+
+        /*requestSpecification = given()
+                .baseUri(ObjectReader.reader.getURI())
+                .header("x-api-key", ObjectReader.reader.getKey());*/
+
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecBuilder.setBaseUri(ObjectReader.reader.getURI());
+        requestSpecBuilder.addHeader("x-api-key", ObjectReader.reader.getKey());
+        requestSpecBuilder.log(LogDetail.ALL);
+
+        //Default RequestSpecification
+        //This static variable will retains its value,
+        // so we can directly use get()
+        RestAssured.requestSpecification = requestSpecBuilder.build();
+
+        responseSpecification = RestAssured.expect()
+                .contentType(ContentType.JSON)
+                .statusCode(200);
+
+    }
+
+    @Test
+
+    public void response_Spec_Demo() {
+
+        get(Endpoints_Web_Services.WORKSPACE)
+                .then()
+                .spec(responseSpecification)
+                .log().all();
+    }
+
+    @Test
+    public void assert_response_Type() {
+
+        Response response = get(Endpoints_Web_Services.WORKSPACE)
+                .then().spec(responseSpecification)
+                .log().all().extract().response();
+
+        assertThat(response.path("workspaces[0].name"), is(equalTo("My Workspace")));
+    }
+
+}
