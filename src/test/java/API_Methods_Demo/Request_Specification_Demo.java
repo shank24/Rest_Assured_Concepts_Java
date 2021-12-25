0ed0769a7d4d8c1b39f85ce830ed50db6b304536
shankkalra <shankkalra@gmail.com>
2021-12-25 23:17:20 +0530

    Request_Spec_Non_BDD_Style

--- src/test/java/API_Methods_Demo/Request_Specification_Demo.java
+++ src/test/java/API_Methods_Demo/Request_Specification_Demo.java
@@ -2,11 +2,15 @@ package API_Methods_Demo;
 
 import com.rest.endpoints.Endpoints_Web_Services;
 import com.rest.propertyReader.ObjectReader;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class Request_Specification_Demo {
     RequestSpecification requestSpecification;
@@ -45,4 +49,21 @@ public class Request_Specification_Demo {
                 .assertThat()
                 .statusCode(200);
     }
+
+    //non-Bdd Style
+    @Test
+    public void assert_Status_Code() {
+
+        Response response = requestSpecification.get(Endpoints_Web_Services.WORKSPACE).then().log().all().extract().response();
+        assertThat(response.statusCode(), is(equalTo(200)));
+    }
+
+    @Test
+    public void assert_Body() {
+
+        Response response = requestSpecification.get(Endpoints_Web_Services.WORKSPACE).then().log().all().extract().response();
+        assertThat(response.path("workspaces[0].name"), is(equalTo("My Workspace")));
+    }
+
+    //My Workspace
 }
