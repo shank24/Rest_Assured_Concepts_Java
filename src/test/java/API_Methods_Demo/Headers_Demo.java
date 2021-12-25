eb9bfb6891fcae4de050894396f994c6da34b87f
shankkalra <shankkalra@gmail.com>
2021-12-25 19:31:32 +0530

    For Each Extract Multiple Response Headers

--- src/test/java/API_Methods_Demo/Headers_Demo.java
+++ src/test/java/API_Methods_Demo/Headers_Demo.java
@@ -2,6 +2,7 @@ package API_Methods_Demo;
 
 import com.rest.endpoints.Endpoints_Web_Services;
 import com.rest.propertyReader.ObjectReader;
+import io.restassured.http.Header;
 import io.restassured.http.Headers;
 import org.testng.annotations.Test;
 
@@ -44,6 +45,30 @@ public class Headers_Demo {
         System.out.println("Extracted Headers Name --> " + extracted_Headers.get("Server").getName());
         System.out.println("Extracted Headers Value --> " + extracted_Headers.get("Server").getValue());
         System.out.println("Extracted Headers Value --> " + extracted_Headers.getValue("Server"));
+
+    }
+
+
+    @Test
+    public void extract_Multiple_Response_Headers() {
+        Headers extracted_Headers = given()
+                .baseUri(ObjectReader.reader.getURI())
+                .header("x-api-key", ObjectReader.reader.getKey())
+                .when()
+                .log().method()
+                .get(Endpoints_Web_Services.WORKSPACE)
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .headers();
+
+        for (Header header : extracted_Headers) {
+            System.out.print("Header Name -->" + header.getName() + " , ");
+            System.out.println("Header Value --> " + header.getValue());
+        }
+
 
     }
 }
