02a22aa5f72626445cef00ac8575ab87112fc707
shankkalra <shankkalra@gmail.com>
2021-12-25 23:05:22 +0530

    Request_Spec_Demo

--- src/test/java/API_Methods_Demo/Request_Specification_Demo.java
+++ src/test/java/API_Methods_Demo/Request_Specification_Demo.java
@@ -3,21 +3,42 @@ package API_Methods_Demo;
 import com.rest.endpoints.Endpoints_Web_Services;
 import com.rest.propertyReader.ObjectReader;
 import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class Request_Specification_Demo {
+    RequestSpecification requestSpecification;
 
-    @Test
-    public void request_Spec_Demo() {
-        RequestSpecification requestSpecification = given()
+    @BeforeClass
+    public void beforeClass() {
+        requestSpecification = given()
                 .baseUri(ObjectReader.reader.getURI())
                 .header("x-api-key", ObjectReader.reader.getKey());
+    }
+
+    @Test
+
+    public void request_Spec_Demo() {
 
         given().spec(requestSpecification)
                 .when()
                 .log().method()
+                .get(Endpoints_Web_Services.WORKSPACE)
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200);
+    }
+
+    @Test
+    public void log_request_Body() {
+
+
+        given().spec(requestSpecification)
+                .when()
+                .log().all()
                 .get(Endpoints_Web_Services.WORKSPACE)
                 .then()
                 .log().all()
