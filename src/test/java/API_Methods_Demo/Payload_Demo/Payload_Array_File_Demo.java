a5cd31fc72fecd5d81cdffd2105b89f3351132d1
shankkalra <shankkalra@gmail.com>
2021-12-31 16:21:46 +0530

    Array_Payload_Demo

--- src/test/java/API_Methods_Demo/Payload_Demo/Payload_Array_File_Demo.java
+++ src/test/java/API_Methods_Demo/Payload_Demo/Payload_Array_File_Demo.java
@@ -8,6 +8,11 @@ import io.restassured.http.ContentType;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -24,6 +29,7 @@ public class Payload_Array_File_Demo {
 
         requestSpecBuilder.setBaseUri("https://7fd638e3-e495-4a96-9c53-2cafad55c9d5.mock.pstmn.io/");
         requestSpecBuilder.setContentType(ContentType.JSON);
+        requestSpecBuilder.addHeader("x-mock-match-request-body", "true");
         requestSpecBuilder.log(LogDetail.ALL);
 
         RestAssured.requestSpecification = requestSpecBuilder.build();
@@ -41,7 +47,21 @@ public class Payload_Array_File_Demo {
 
     @Test
     public void validate_Post_Verb_Payload_From_Array() {
+        HashMap<String, String> obj5001 = new HashMap<>();
+        obj5001.put("id", "5001");
+        obj5001.put("type", "None");
+
+        HashMap<String, String> obj5002 = new HashMap<>();
+        obj5002.put("id", "5002");
+        obj5002.put("type", "Cherry");
+
+        List<Map> jsonList = new ArrayList<>();
+        jsonList.add(obj5001);
+        jsonList.add(obj5002);
+
+
         given()
+                .body(jsonList)
                 .when()
                 .post("/post")
                 .then()
