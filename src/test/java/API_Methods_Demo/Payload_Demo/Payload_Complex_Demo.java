474195ed27f62a9c0e9431aaa9b63b6301fa3a9b
shankkalra <shankkalra@gmail.com>
2021-12-31 19:19:43 +0530

    Complex Json

---
+++ src/test/java/API_Methods_Demo/Payload_Demo/Payload_Complex_Demo.java
@@ -0,0 +1,156 @@
+package API_Methods_Demo.Payload_Demo;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.config.EncoderConfig;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static io.restassured.RestAssured.config;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+
+public class Payload_Complex_Demo {
+
+    @BeforeClass
+    public void beforeClass() {
+
+        /**
+         * RequestSpec Builder
+         */
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+
+        requestSpecBuilder.setBaseUri("https://7fd638e3-e495-4a96-9c53-2cafad55c9d5.mock.pstmn.io/");
+        requestSpecBuilder.setContentType(ContentType.JSON);
+        requestSpecBuilder.addHeader("x-mock-match-request-body", "true");
+        requestSpecBuilder.setConfig(config.encoderConfig(EncoderConfig.encoderConfig().
+                appendDefaultContentCharsetToContentTypeIfUndefined(false)));
+        requestSpecBuilder.log(LogDetail.ALL);
+
+        RestAssured.requestSpecification = requestSpecBuilder.build();
+
+        /**
+         * ResponseSpec Builder
+         */
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectContentType(ContentType.JSON)
+                .log(LogDetail.ALL);
+
+        RestAssured.responseSpecification = responseSpecBuilder.build();
+    }
+
+
+    @Test
+    public void validate_Post_Verb_Payload_From_Complex_Json() {
+
+        //Batter Payload
+        List<Integer> idArrayList = new ArrayList<>();
+
+        idArrayList.add(5);
+        idArrayList.add(9);
+
+        HashMap<String, Object> batterHashMap2 = new HashMap<>();
+
+        batterHashMap2.put("id", idArrayList);
+        batterHashMap2.put("type", "Chocolate");
+
+        HashMap<String, Object> batterHashMap1 = new HashMap<>();
+
+        batterHashMap1.put("id", "1001");
+        batterHashMap1.put("type", "Regular");
+
+        List<HashMap<String, Object>> batterArrayList = new ArrayList<>();
+        batterArrayList.add(batterHashMap1);
+        batterArrayList.add(batterHashMap2);
+
+        HashMap<String, List<HashMap<String, Object>>> batterHashMap = new HashMap<>();
+        batterHashMap.put("batter", batterArrayList);
+
+        //Topping Payload
+
+        List<String> typeArrayList = new ArrayList<>();
+        typeArrayList.add("test1");
+        typeArrayList.add("test2");
+
+
+        HashMap<String, Object> toppingHashMap2 = new HashMap<>();
+
+        toppingHashMap2.put("id", "5002");
+        toppingHashMap2.put("type", typeArrayList);
+
+        HashMap<String, Object> toppingHashMap1 = new HashMap<>();
+
+        toppingHashMap1.put("id", "5001");
+        toppingHashMap1.put("type", "None");
+
+        List<HashMap<String, Object>> toppingArrayList = new ArrayList<>();
+        toppingArrayList.add(toppingHashMap1);
+        toppingArrayList.add(toppingHashMap2);
+
+        HashMap<String, Object> mainHashMap = new HashMap<>();
+        mainHashMap.put("id", "0001");
+        mainHashMap.put("type", "donut");
+        mainHashMap.put("name", "Cake");
+        mainHashMap.put("ppu", 0.55);
+        mainHashMap.put("batters", batterHashMap);
+        mainHashMap.put("topping", toppingArrayList);
+
+        given()
+                .body(mainHashMap)
+                .when()
+                .post("/postComplexJson")
+                .then()
+                .log()
+                .all()
+                .assertThat()
+                .body("msg", equalTo("Success"));
+
+    }
+
+}
+
+/*
+    {
+   "id":"0001",
+   "type":"donut",
+   "name":"Cake",
+   "ppu":0.55,
+   "batters":{
+      "batter":[
+         {
+            "id":"1001",
+            "type":"Regular"
+         },
+         {
+            "id":[
+               5,
+               9
+            ],
+            "type":"Chocolate"
+         }
+      ]
+   },
+   "topping":[
+      {
+         "id":"5001",
+         "type":"None"
+      },
+      {
+         "id":"5002",
+         "type":[
+            "test1",
+            "test2"
+         ]
+      }
+   ]
+}
+        */
