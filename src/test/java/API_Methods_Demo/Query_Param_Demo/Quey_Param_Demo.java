20d03541823369aafffc9e9b15ee60c3788db3aa
shankkalra <shankkalra@gmail.com>
2022-01-02 15:31:02 +0530

    Query Param

---
+++ src/test/java/API_Methods_Demo/Query_Param_Demo/Quey_Param_Demo.java
@@ -0,0 +1,54 @@
+package API_Methods_Demo.Query_Param_Demo;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+
+public class Quey_Param_Demo {
+
+    @BeforeClass
+    public void beforeClass() {
+
+        /**
+         * RequestSpec Builder
+         */
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+
+        requestSpecBuilder.setBaseUri("https://postman-echo.com/");
+        requestSpecBuilder.setContentType(ContentType.JSON);
+        requestSpecBuilder.log(LogDetail.ALL);
+
+        RestAssured.requestSpecification = requestSpecBuilder.build();
+
+        /**
+         * ResponseSpec Builder
+         */
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectContentType(ContentType.JSON)
+                .log(LogDetail.ALL);
+
+        RestAssured.responseSpecification = responseSpecBuilder.build();
+    }
+
+    @Test
+    public void validate_Query_Param() {
+
+        given()
+                .param("foo1", "bar1")
+                .when()
+                .get("/get")
+                .then()
+                .log()
+                .all();
+    }
+
+
+}
