7dc96332b3d784e5ad5189e8a47190260a1d3343
shankkalra <shankkalra@gmail.com>
2022-01-02 19:24:25 +0530

    Upload_File_Demo

---
+++ src/test/java/API_Methods_Demo/Multipart_Demo/Upload_File_Demo.java
@@ -0,0 +1,57 @@
+package API_Methods_Demo.Multipart_Demo;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+
+
+public class Upload_File_Demo {
+
+    @BeforeClass
+    public void beforeClass() {
+
+        /**
+         * RequestSpec Builder
+         */
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+
+        requestSpecBuilder.setBaseUri("https://postman-echo.com/");
+        requestSpecBuilder.setContentType(ContentType.MULTIPART);
+        requestSpecBuilder.log(LogDetail.ALL);
+
+        RestAssured.requestSpecification = requestSpecBuilder.build();
+
+        /**
+         * ResponseSpec Builder
+         */
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectContentType(ContentType.JSON)
+                .log(LogDetail.ALL);
+
+        RestAssured.responseSpecification = responseSpecBuilder.build();
+    }
+
+    @Test
+    public void validate_Path_Param() {
+
+        String attributes = "{\"name\" : \"File\"}";
+        given()
+                .multiPart("file", new File("Dummy_File"))
+                .multiPart("attributes", attributes, "application/json")
+                .when()
+                .post("/post")
+                .then()
+                .log()
+                .all();
+    }
+
+}
