ebb5f50a4d58f8b12c8e682947fda7722ecec87d
shankkalra <shankkalra@gmail.com>
2022-01-02 19:09:43 +0530

    MultiPart Demo

---
+++ src/test/java/API_Methods_Demo/Multipart_Demo/Multipart_Demo.java
@@ -0,0 +1,53 @@
+package API_Methods_Demo.Multipart_Demo;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+
+public class Multipart_Demo {
+
+    @BeforeClass
+    public void beforeClass() {
+
+        /**
+         * RequestSpec Builder
+         */
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+
+        requestSpecBuilder.setBaseUri("https://postman-echo.com/");
+        requestSpecBuilder.setContentType(ContentType.MULTIPART);
+        requestSpecBuilder.log(LogDetail.ALL);
+
+        RestAssured.requestSpecification = requestSpecBuilder.build();
+
+        /**
+         * ResponseSpec Builder
+         */
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectContentType(ContentType.JSON)
+                .log(LogDetail.ALL);
+
+        RestAssured.responseSpecification = responseSpecBuilder.build();
+    }
+
+    @Test
+    public void validate_Path_Param() {
+
+        given()
+                .multiPart("foo1", "bar1")
+                .when()
+                .post("/post")
+                .then()
+                .log()
+                .all();
+    }
+
+}
