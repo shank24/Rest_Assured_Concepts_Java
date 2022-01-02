c4f5a96dd531e8e667e05debf5af51aee319db0d
shankkalra <shankkalra@gmail.com>
2022-01-02 23:28:51 +0530

    Logging Filter With File

--- src/test/java/API_Methods_Demo/Filter_Demo/Filter_Demo.java
+++ src/test/java/API_Methods_Demo/Filter_Demo/Filter_Demo.java
@@ -5,6 +5,10 @@ import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 import static io.restassured.RestAssured.given;
 
 
@@ -23,6 +27,23 @@ public class Filter_Demo {
                 .get("/get")
                 .then()
                 //.log().all()
+                .assertThat()
+                .statusCode(200);
+    }
+
+
+    @Test
+    public void validate_Filter_Demo_Log_File() throws FileNotFoundException {
+
+        PrintStream FileOutputStream = new PrintStream(new File("restAssured.log"));
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .filter(new RequestLoggingFilter(FileOutputStream))
+                .filter(new ResponseLoggingFilter(FileOutputStream))
+                .when()
+                .get("/get")
+                .then()
                 .assertThat()
                 .statusCode(200);
     }
