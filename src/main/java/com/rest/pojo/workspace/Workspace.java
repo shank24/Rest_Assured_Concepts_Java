4398451c5ad6bb9d55a5077fe67b331ab31f867a
shankkalra <shankkalra@gmail.com>
2022-01-11 22:04:37 +0530

    Json Include Prop

--- src/main/java/com/rest/pojo/workspace/Workspace.java
+++ src/main/java/com/rest/pojo/workspace/Workspace.java
@@ -1,5 +1,7 @@
 package com.rest.pojo.workspace;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,16 +10,25 @@ import java.util.HashMap;
 
 @Getter
 @Setter
+@JsonIgnoreProperties(value = "id", allowSetters = true)
+
+/*
+If we want to use property during
+Serialization - allowgetters = true
+De-Serialization - allowsetters = true
+*/
+
 
 public class Workspace {
     private String name;
     private String type;
     private String description;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
+    //@JsonInclude(JsonInclude.Include.NON_NULL)
     private String id;
 
-    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    //@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    @JsonIgnore
     private int i;
 
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
