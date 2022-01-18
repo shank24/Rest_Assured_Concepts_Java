9ba60e5d7e5f2901aa0e85de48436057d68ef3fe
shankkalra <shankkalra@gmail.com>
2022-01-18 13:12:45 +0530

    Info CLass Getter/Setter

--- src/main/java/com/rest/pojo/complexPojo/Info.java
+++ src/main/java/com/rest/pojo/complexPojo/Info.java
@@ -1,5 +1,10 @@
 package com.rest.pojo.complexPojo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Info {
     private String name;
     private String description;
