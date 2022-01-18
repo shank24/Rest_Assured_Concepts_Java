0e0fccd7439e30bb1565b142012030b88a7199b7
shankkalra <shankkalra@gmail.com>
2022-01-18 13:25:39 +0530

    Collection Class

--- src/main/java/com/rest/pojo/complexPojo/Collection.java
+++ src/main/java/com/rest/pojo/complexPojo/Collection.java
@@ -1,6 +1,14 @@
 package com.rest.pojo.complexPojo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class Collection {
     Info info;
+    List<Object> item;
 
 }
--- src/main/java/com/rest/pojo/complexPojo/Info.java
+++ src/main/java/com/rest/pojo/complexPojo/Info.java
@@ -10,4 +10,14 @@ public class Info {
     private String description;
     private String schema;
 
+    public Info(String name, String description, String schema) {
+        this.name = name;
+        this.description = description;
+        this.schema = schema;
+    }
+
+    public Info() {
+
+    }
+
 }
