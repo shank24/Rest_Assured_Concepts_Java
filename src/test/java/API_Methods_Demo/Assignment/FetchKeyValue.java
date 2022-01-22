48af32606843752b3f8dd560d110e179e398f3da
shankkalra <shankkalra@gmail.com>
2022-01-22 21:07:33 +0530

    Json Assert Lib

--- src/test/java/API_Methods_Demo/Assignment/FetchKeyValue.java
+++ src/test/java/API_Methods_Demo/Assignment/FetchKeyValue.java
@@ -10,6 +10,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+//Assignment
 public class FetchKeyValue {
     private static final List<String> keys = new ArrayList<>();
     private static final List<Object> values = new ArrayList<>();
